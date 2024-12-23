781e1bc652695af4be39042f925bbefcf0f5aa3f
Levvikosta <levikosta2015@yandex.ru>
2024-12-23 15:12:05 +0300

    added test directory

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def divide(a, b):
+    """"функция деления"""
+    if a > b:
+        return a / b
+    return 0
+
+
+def reverse_string(my_string):
+    """"разворачивает строку"""
+    return my_string[::-1]
